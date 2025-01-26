80e56810f6073a7d4a6bc9cac2cc92709a9b5382
piyushb790 <piyushb790@gmail.com>
2025-01-26 14:53:06 +0530

    Resolved CORS issue

--- src/constants.jsx
+++ src/constants.jsx
@@ -2,7 +2,7 @@ export const IMG_CDN_LINK =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_250,w_350/";
 
 export const RES_MENU_CDN =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3293562&lng=78.0539496&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3293562&lng=78.0539496&restaurantId=";
 
 export const ALL_RES_CDN =
   "https://cors-handlers.vercel.app/api/?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D30.3293562%26lng%3D78.0539496%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
