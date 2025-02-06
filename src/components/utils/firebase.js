1bfdac63de674b70a496ef1353b61312c6e5626a
Piyush <piyushb790@gmail.com>
2025-02-06 22:52:19 +0530

    fixing .env issues

--- src/components/utils/firebase.js
+++ src/components/utils/firebase.js
@@ -1,14 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey = import.meta.env.VITE_API_KEY;
 const firebaseConfig = {
-  apiKey: "AIzaSyDeNkeWex5iCDHAXs3CAJQfiqzpoqApNjg",
+  apiKey: apiKey,
   authDomain: "doondelights.firebaseapp.com",
   projectId: "doondelights",
   storageBucket: "doondelights.firebasestorage.app",
@@ -18,6 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 export const auth = getAuth();
